aed8691a71a04449760eb26ed06520c28ec6f94c
gegyhamdani <gegyhamdani@gmail.com>
2020-09-15 21:07:31 +0700

    create an axios instance

---
+++ src/services/axiosInstance.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const apiUrlBase = "http://www.omdbapi.com";
+
+const apiKey = "faf7e5bb&s";
+
+const apiUrl = `${apiUrlBase}/?apikey=${apiKey}`;
+
+const errorResponseHandler = (error) => {
+  return Promise.reject(error);
+};
+
+const getInstance = () => {
+  const instance = axios.create({
+    baseURL: apiUrl,
+    timeout: 10000,
+  });
+
+  instance.interceptors.response.use(
+    (response) => response,
+    errorResponseHandler
+  );
+  return instance;
+};
+
+const routes = {
+  movie: (title, page) => `&s=${title}&page=${page}`,
+};
+
+export default {
+  getInstance,
+  routes,
+};
